ec659db141a1ae568c24459b58b1ab69ea200ddf
QiZhong <76236177+ChungChyi@users.noreply.github.com>
2022-08-02 16:17:29 +0800

    Update __init__.py

--- math_demo/__init__.py
+++ math_demo/__init__.py
@@ -1,4 +1,22 @@
-<<<<<<< HEAD
+
+#定义个Class，包含两个数字加法运算的方法
+
+class my_math:
+    def __init__(self,num1,num2):
+        self.num1 = int(num1)
+        self.num2 = int(num2)
+    def SumTwoNums(self):
+        return self.num1 + self.num2
+    def MulTwoNums(self):
+        return self.num1 * self.num2
+    def SubTwoNums(self):
+        return self.num1 - self.num2
+    def DivTwoNums(self):
+        if self.num2 != 0:
+            return self.num1/self.num2
+        else:
+            return 'The divisor can not be zero!!!'
+
 #定义个Class，包含两个数字加法运算的方法
 
 class my_math:
@@ -19,25 +37,3 @@ class my_math:
 
 
 
-=======
-#定义个Class，包含两个数字加法运算的方法
-
-class my_math:
-    def __init__(self,num1,num2):
-        self.num1 = int(num1)
-        self.num2 = int(num2)
-    def SumTwoNums(self):
-        return self.num1 + self.num2
-    def MulTwoNums(self):
-        return self.num1 * self.num2
-    def SubTwoNums(self):
-        return self.num1 - self.num2
-    def DivTwoNums(self):
-        if self.num2 != 0:
-            return self.num1/self.num2
-        else:
-            return 'The divisor can not be zero!!!'
-
-
-
->>>>>>> c4fd25b1dd19835b5afa11c33fd25fa7bfc9bea1
